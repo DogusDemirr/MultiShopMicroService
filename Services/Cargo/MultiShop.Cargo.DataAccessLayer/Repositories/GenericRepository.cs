421644a7de46726274e3509c941b5200495dacc8
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2025-04-22 21:39:57 +0300

    Cargo generic repository

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly CargoContext _context;
+
+        public GenericRepository(CargoContext context)
+        {
+            _context = context;
+        }
+        public void Delete(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Id değeri 0");
+            }
+            var values = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(values);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            var values = _context.Set<T>().ToList();
+            return values;
+        }
+
+        public T GetById(int id)
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentException("Id değeri 0");
+            }
+            var value = _context.Set<T>().Find(id);
+            return value;
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}

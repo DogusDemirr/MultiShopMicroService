651793c8c430c3eefb26f6ceb6227a3dcb37882f
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2025-04-22 17:09:50 +0300

    order token

--- Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -4,8 +4,16 @@ using MultiShop.Order.Application.Interfaces;
 using MultiShop.Order.Persistence.Repositories;
 using MultiShop.Order.Application.Services;
 using MultiShop.Order.Persistence.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "ResourceOrder";
+    opt.RequireHttpsMetadata = false;
+});
 
 builder.Services.AddDbContext<OrderContext>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
@@ -41,6 +49,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

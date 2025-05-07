9c31c5697aacdb41366e0dc6d8d26a6711009419
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2025-05-07 17:21:20 +0300

    Redis servis sınıfı

---
+++ Services/Basket/MultiShop.Basket/Settings/RedisService.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+
+namespace MultiShop.Basket.Settings
+{
+    public class RedisService
+    {
+        private readonly string _host;
+
+        private readonly int _port;
+
+        private ConnectionMultiplexer _connectionMultiplexer; //Redis e bağlanmak için köprü görevi görecek
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+
+        public void GetDb(int db = 1) => _connectionMultiplexer.GetDatabase(0);
+
+        //public IDatabase GetDb(int dbIndex = 1)
+        //{
+        //    if (_connectionMultiplexer == null)
+        //        throw new Exception("Redis connection is not established.");
+        //    return _connectionMultiplexer.GetDatabase(dbIndex);
+        //}
+    }
+}
